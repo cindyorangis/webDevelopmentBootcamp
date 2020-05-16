33e53d22ed3853114150145875ae5e14e88d42a0
Cindy Le <cindyledev@gmail.com>
2020-05-16 17:11:31 -0400

    If-Else Conditions

--- IntroductionToJavaScriptES6/loveCalculator.js
+++ IntroductionToJavaScriptES6/loveCalculator.js
@@ -3,4 +3,15 @@ prompt("What is their name?");
 
 var loveScore = Math.random() * 100;
 loveScore = Math.floor(loveScore) + 1;
-alert("Your love score is " + loveScore);
+
+if (loveScore === 100) {
+  alert(
+    "Your love score is " +
+      loveScore +
+      "%. You love each other like Kanye loves Kanye."
+  );
+} else if (loveScore > 70 && loveScore < 100) {
+  alert("Your love score is " + loveScore + "%. Good enough.");
+} else {
+  alert("Your love score is " + loveScore + "%");
+}
--- README.md
+++ README.md
@@ -8,4 +8,8 @@
 - 124: Functions Part 2: Parameters and Arguments
 - Coding Exercise 3: Life in Weeks
 - Coding Exercise 4: BMI Calculator
+
+## Section 10: Intermediate JavaScript
+
 - 131: Random Number Generation in Javascript: Building a Love Calculator
+- 132: Control Statements: Using If-Else Conditionals & Logic
