79d453d31db894c6e127a527885905aa90f3807d
Cindy Le <cindyledev@gmail.com>
2020-05-16 13:58:49 -0400

    Functions Part 3: Outputs & Return Values

--- IntroductionToJavaScriptES6/parametersAndArguments.js
+++ IntroductionToJavaScriptES6/parametersAndArguments.js
@@ -1,7 +1,20 @@
 function getMilk(money, cost) {
-  var costOfBottle = cost;
-  var numberOfBottles = Math.floor(money / costOfBottle);
+  var numberOfBottles = Math.floor(money / cost);
   console.log("buy " + numberOfBottles + " bottles of milk");
+  return numberOfBottles;
+}
+
+function getChange(money, cost) {
+  var change = money % cost;
+  console.log();
+  return change;
 }
 
 getMilk(5, 1.5);
+
+console.log(
+  "bought " +
+    getMilk(5, 1.5) +
+    " bottles of milk, and your change of $" +
+    getChange(5, 1.5)
+);
