f0397a41daae7d06cb461dec2f4db5feab180c44
Cindy Le <cindyledev@gmail.com>
2020-05-18 02:36:27 -0400

    Step 5 - Change both <img> Elements

--- Dicee Challenge/index.js
+++ Dicee Challenge/index.js
@@ -1,6 +1,15 @@
+// Die 1
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-var randomImageSource = "images/dice" + randomNumber1 + ".png";
+var randomImageSource1 = "images/dice" + randomNumber1 + ".png";
 
 var image1 = document.querySelectorAll("img")[0];
-image1.setAttribute("src", randomImageSource);
+image1.setAttribute("src", randomImageSource1);
+
+// Die 2
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomImageSource2);
