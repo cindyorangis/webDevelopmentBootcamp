30c84ec948eef2a8471320c06a4e5eed8d2644c8
Cindy Le <cindyledev@gmail.com>
2020-05-20 01:43:49 -0400

    Change drum color to white

--- Advanced JavaScript and DOM Manipulation/Drum Kit/index.js
+++ Advanced JavaScript and DOM Manipulation/Drum Kit/index.js
@@ -1,6 +1,9 @@
 // Adding Event Listeners to Drum Buttons
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", () => {
-    alert("I got clicked");
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    // var audio = new Audio("sounds/tom-1.mp3");
+    // audio.play();
+    console.log(this);
+    this.style.color = "white";
   });
 }
