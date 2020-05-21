930702443287ba461a097072ac18b8e21edea60c
Cindy Le <cindyledev@gmail.com>
2020-05-21 08:51:13 -0400

    Show the Sequence to the User with Animations and Sounds

--- Simon Game Challenge/game.js
+++ Simon Game Challenge/game.js
@@ -1,8 +1,23 @@
+// Array for button colours
 var buttonColours = ["red", "blue", "green", "yellow"];
+// Initialize game pattern array;
 var gamePattern = [];
 
 function nextSequence() {
+  // Generates a random number between 0-3
   var randomNumber = Math.round(Math.random() * 3);
+  // Select random colour from buttonColurs array
   var randomChosenColour = buttonColours[randomNumber];
+  // Add new randomChosenColur to end of gamePattern array
   gamePattern.push(randomChosenColour);
+
+  // Animate randomChosenColour
+  $("#" + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  // Play sound for button
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
 }
