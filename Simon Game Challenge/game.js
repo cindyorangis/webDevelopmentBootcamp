d8d5d9aa1ee46b1b9073c475e2ca7c9589c693c1
Cindy Le <cindyledev@gmail.com>
2020-05-21 08:28:08 -0400

    Step 2 - Create a New Pattern

--- Simon Game Challenge/game.js
+++ Simon Game Challenge/game.js
@@ -1,1 +1,8 @@
-alert("Working");
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+
+function nextSequence() {
+  var randomNumber = Math.round(Math.random() * 3);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+}
