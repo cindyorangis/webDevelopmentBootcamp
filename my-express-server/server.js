3125e1d2a05faae87056981ce67bf70919bfac1b
Cindy Le <cindyledev@gmail.com>
2020-05-22 00:08:00 -0400

    created node server

---
+++ my-express-server/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>");
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
