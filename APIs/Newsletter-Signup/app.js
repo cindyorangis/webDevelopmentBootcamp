f9ed861e8ca9c645fb83b6bf7235ca6a74371647
Cindy Le <cindyledev@gmail.com>
2020-05-23 02:57:00 -0400

    Setting up the Sign Up Page

--- APIs/Newsletter-Signup/app.js
+++ APIs/Newsletter-Signup/app.js
@@ -3,12 +3,21 @@ const bodyParser = require("body-parser");
 const port = 3000;
 const app = express();
 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-  res.send();
+  res.sendFile(__dirname + "/signup.html");
 });
 
-app.listen(port, function () {
+app.post("/", function (req, res) {
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+
+  console.log(firstName, lastName, email);
+});
+
+app.listen(port, () => {
   console.log("Server is running on port " + port);
 });
