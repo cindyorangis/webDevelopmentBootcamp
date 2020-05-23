09480a4d8f907cd8434f4a02eb911c5cbe2df80d
Cindy Le <cindyledev@gmail.com>
2020-05-22 23:45:32 -0400

    Parse JSON data

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -7,7 +7,22 @@ app.get("/", function (req, res) {
   const url =
     "https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=ca7c312b1af4511855cc6c0fb1bd8e8e&units=metric";
   https.get(url, function (response) {
-    console.log(response);
+    console.log(response.statusCode);
+
+    response.on("data", function (data) {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp; // main.temp
+      const description = weatherData.weather[0].description; // weather[0].description
+      console.log(description);
+
+      /*
+      const obj = {
+        name: "Cindy",
+        favouriteFood: "Ramen",
+      };
+      console.log(JSON.stringify(obj));
+      */
+    });
   });
   res.send("Server is up and running");
 });
