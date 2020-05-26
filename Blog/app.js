183231226095047c0863b4e185f4e39e45bb2a20
Cindy Le <cindyledev@gmail.com>
2020-05-26 00:24:09 -0400

    add compose form and POST route

--- Blog/app.js
+++ Blog/app.js
@@ -30,6 +30,14 @@ app.get("/contact", (req, res) => {
   res.render("contact", { contact: contactContent });
 });
 
+app.get("/compose", (req, res) => {
+  res.render("compose");
+});
+
+app.post("/compose", (req, res) => {
+  console.log(req.body.publishText);
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
