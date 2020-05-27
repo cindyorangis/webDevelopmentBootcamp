60ca35ca31e565852e898a2e9493b0762442181f
Cindy Le <cindyledev@gmail.com>
2020-05-27 12:26:00 -0400

    add lodash and use _.lowerCase on titles

--- Blog/app.js
+++ Blog/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+var _ = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -51,13 +52,15 @@ app.post("/compose", (req, res) => {
 });
 
 app.get("/posts/:blogPost", (req, res) => {
-  const requestedTitle = req.params.blogPost;
+  const requestedTitle = _.lowerCase(req.params.blogPost);
 
   posts.forEach((post) => {
-    const storedTitle = post.title;
+    const storedTitle = _.lowerCase(post.title);
 
     if (storedTitle === requestedTitle) {
       console.log("Match Found!");
+    } else {
+      console.log("Not a Match");
     }
   });
 });
