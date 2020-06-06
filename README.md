3367e2c2ab1326b01aaa9b3673d9d26b7c58cfa7
Cindy Le <cindyledev@gmail.com>
2020-06-06 00:57:56 -0400

    Level 5 - Using Passport.js to Add Cookies and Sessions

--- README.md
+++ README.md
@@ -162,3 +162,4 @@
 - 407: Using Environment Variables to Keep Secrets Safe
 - 408: Level 3 - Hashing Passwords
 - 410: Level 4 - Salting and Hashing Passwords with bcrypt
+- 412: Level 5 - Using Passport.js to Add Cookies and Sessions
--- Secrets/app.js
+++ Secrets/app.js
@@ -5,8 +5,11 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 // const encrypt = require("mongoose-encryption");
 // const md5 = require("md5");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+// const bcrypt = require("bcrypt");
+// const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const port = 3000;
 
@@ -14,6 +17,15 @@ const app = express();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set("view engine", "ejs");
 
@@ -21,6 +33,7 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
   email: String,
@@ -35,7 +48,13 @@ userSchema.plugin(encrypt, {
 });
 */
 
+userSchema.plugin(passportLocalMongoose);
+
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -49,6 +68,8 @@ app.get("/register", (req, res) => {
   res.render("register");
 });
 
+/*
+// Using bcrypt
 app.post("/register", (req, res) => {
   bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
     const newUser = new User({
@@ -85,6 +106,54 @@ app.post("/login", (req, res) => {
       }
     }
   });
+});
+*/
+
+app.get("/secrets", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.post("/register", (req, res) => {
+  User.register(
+    { username: req.body.username },
+    req.body.password,
+    (err, user) => {
+      if (err) {
+        console.log(err);
+        res.redirect("/register");
+      } else {
+        passport.authenticate("local")(req, res, () => {
+          res.redirect("/secrets");
+        });
+      }
+    }
+  );
+});
+
+app.post("/login", (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  req.login(user, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, () => {
+        res.redirect("/secrets");
+      });
+    }
+  });
+});
+
+app.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
 });
 
 app.listen(port, () => {
